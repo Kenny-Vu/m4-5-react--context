5cd28a7c2691b9dc06c24cc1ac4314b42da44397
Kenny Vu <kennyvu@live.ca>
2020-07-27 18:00:10 -0400

    Exercise 1 - Items are also persistant now

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import GlobalStyles from "./GlobalStyles";
@@ -8,30 +8,34 @@ import items from "../data";
 
 //CUSTOM HOOK - LOCAL STORAGE
 const usePersistedState = (keyName, keyValue) => {
+  //if value is already in storage, use that value instead
   const trueKeyValue = window.localStorage.getItem(keyName)
-    ? Number(window.localStorage.getItem(keyName))
+    ? JSON.parse(`${window.localStorage.getItem(keyName)}`)
     : keyValue;
-  console.log(window.localStorage.getItem(keyName));
+  //create new state and pass the previous value or keyValue argument
   const [state, setState] = React.useState(trueKeyValue);
+  //update local storage everytime state changes
   React.useEffect(() => {
-    window.localStorage.setItem(keyName, state);
+    window.localStorage.setItem(keyName, JSON.stringify(state));
   }, [state]);
-  return [state, setState];
+  //returning state
+  return [trueKeyValue, setState];
 };
 
 function App(props) {
   // const [numCookies, setNumCookies] = React.useState(1000);
-  const [purchasedItems, setPurchasedItems] = React.useState({
-    cursor: 0,
-    grandma: 0,
-    farm: 0,
-  });
-  const [numCookies, setNumCookies] = usePersistedState("num-Cookies", 1000);
-  // const [purchasedItems, setPurchasedItems] = usePersistedState("num-Items", {
+  // const [purchasedItems, setPurchasedItems] = React.useState({
   //   cursor: 0,
   //   grandma: 0,
   //   farm: 0,
   // });
+
+  const [numCookies, setNumCookies] = usePersistedState("num-Cookies", 1000);
+  const [purchasedItems, setPurchasedItems] = usePersistedState("num-items", {
+    cursor: 0,
+    grandma: 0,
+    farm: 0,
+  });
 
   return (
     <>
