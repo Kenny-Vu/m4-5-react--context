c43ea4613cd14cd037819881a9721c842dac5efc
Kenny Vu <kennyvu@live.ca>
2020-07-28 14:20:21 -0400

    Exercise 1 - Cookies are now accumulated while the game is closed

--- src/components/App.js
+++ src/components/App.js
@@ -9,17 +9,18 @@ import items from "../data";
 //CUSTOM HOOK - LOCAL STORAGE
 const usePersistedState = (keyName, keyValue) => {
   //if value is already in storage, use that value instead
-  const trueKeyValue = window.localStorage.getItem(keyName)
-    ? JSON.parse(`${window.localStorage.getItem(keyName)}`)
+  const trueKeyValue = JSON.parse(window.localStorage.getItem(keyName))
+    ? JSON.parse(window.localStorage.getItem(keyName))
     : keyValue;
   //create new state and pass the previous value or keyValue argument
   const [state, setState] = React.useState(trueKeyValue);
-  //update local storage everytime state changes
-  React.useEffect(() => {
+  //update state in local storage everytime state changes
+  useEffect(() => {
     window.localStorage.setItem(keyName, JSON.stringify(state));
   }, [state]);
+
   //returning state
-  return [trueKeyValue, setState];
+  return [state, setState];
 };
 
 function App(props) {
@@ -29,14 +30,38 @@ function App(props) {
   //   grandma: 0,
   //   farm: 0,
   // });
-
-  const [numCookies, setNumCookies] = usePersistedState("num-Cookies", 1000);
+  const [numCookies, setNumCookies] = usePersistedState("num-cookies", 1000);
   const [purchasedItems, setPurchasedItems] = usePersistedState("num-items", {
     cursor: 0,
     grandma: 0,
     farm: 0,
   });
 
+  //useEffect to compare timeStamps on mount
+  useEffect(() => {
+    const currentTimeStamp = new Date().getTime();
+    const timeDiff = Math.floor(
+      (currentTimeStamp - JSON.parse(localStorage.getItem("pastTimeStamp"))) /
+        1000
+    );
+    let totalCookiesAcc = null;
+    const itemsPurchasedKeys = Object.keys(purchasedItems);
+    itemsPurchasedKeys.forEach((item) => {
+      let numOwned = purchasedItems[item];
+      let itemValue = items.find((element) => element.id === item).value;
+      totalCookiesAcc += numOwned * itemValue * timeDiff;
+      console.log("current item:" + item);
+      console.log("timeDiff:" + timeDiff);
+      console.log("num of cookies accumulated:" + totalCookiesAcc);
+      console.log("expected num of cookies:" + (numCookies + totalCookiesAcc));
+      console.log("num of cookies:" + numCookies);
+    });
+    setNumCookies(numCookies + totalCookiesAcc);
+  }, []);
+  //useEffect to store current timeStamp
+  useEffect(() => {
+    localStorage.setItem("pastTimeStamp", JSON.stringify(new Date().getTime()));
+  }, [numCookies]);
   return (
     <>
       <GlobalStyles />
